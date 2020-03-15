8b844fdab2b2c1a3f76ceb0014488319b9238d39
alex sarnese <alexthepokemon@Gmail.com>
2020-03-15 09:19:55 -0400

    removed import

--- src/main/java/net/telepathicgrunt/subterranean/blocks/STBlocks.java
+++ src/main/java/net/telepathicgrunt/subterranean/blocks/STBlocks.java
@@ -2,7 +2,6 @@ package net.telepathicgrunt.subterranean.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.telepathicgrunt.subterranean.Subterranean;
